f7bc5a2fa4eb713fee0b385c850f8a5637726b11
unknown <baaanhaaa@gmail.com>
2026-07-24 14:25:02 +0700

    fix bootstrap cache directory write permission on vercel

--- api/index.php
+++ api/index.php
@@ -9,6 +9,7 @@ $directories = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
 ];
 
 foreach ($directories as $dir) {
@@ -32,6 +33,12 @@ $_SERVER['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 $_ENV['APP_DEBUG'] = 'true';
 $_SERVER['APP_DEBUG'] = 'true';
 
+// Override bootstrap cache paths for Vercel
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/bootstrap/cache/events.php';
 
 require __DIR__.'/../vendor/autoload.php';
 
