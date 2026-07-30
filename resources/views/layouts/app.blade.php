d90ff65fb0b15a1ebf8bfc47e7b9e9d27989b4a8
unknown <baaanhaaa@gmail.com>
2026-07-30 12:00:54 +0700

    Restore theme-default.css for Sneat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
 
     <!-- Core CSS -->
     <link rel="stylesheet" href="{{ asset('sneat/vendor/css/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('sneat/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{ asset('sneat/css/demo.css') }}" />
     <link rel="stylesheet" href="{{ asset('sneat/css/dark.css') }}" />
 
